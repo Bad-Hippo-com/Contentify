b68798f95c201f3e4288354634129e884c34d633
Sebastian J. H. <37085865+kamohy@users.noreply.github.com>
2026-09-08 10:31:13 +0200

    fix: Matchergebnisse mit explizitem Fremdschluessel laden

--- app/Modules/Matches/GameMatch.php
+++ app/Modules/Matches/GameMatch.php
@@ -93,7 +93,7 @@ class GameMatch extends BaseModel
     ];
 
     public static $relationsData = [
-        'matchScores'  => [self::HAS_MANY, 'App\Modules\Matches\MatchScore'], // Not a strong dependency.
+        'matchScores'  => [self::HAS_MANY, 'App\Modules\Matches\MatchScore', 'foreignKey' => 'match_id'], // Not a strong dependency.
         'game'         => [self::BELONGS_TO, 'App\Modules\Games\Game'],
         'tournament'   => [self::BELONGS_TO, 'App\Modules\Tournaments\Tournament'],
         'leftTeam'     => [self::BELONGS_TO, 'App\Modules\Teams\Team'],
